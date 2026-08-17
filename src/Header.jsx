f4eff9173050efaddac61c194d7e926d19addf67
gitDevelopment2020 <sonikgh@gmail.com>
2026-08-17 19:24:02 +0300

    Alpha version social network

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRTWDq2p3-0gDoFyfIhJdWK6wUfkNzCsZzVj407gDyGUitzjnTP" />
+    </header>
+  )
+}
+
+export default Header
---
+++ src/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://ianimal.ru/wp-content/uploads/2010/12/smeshnue-kartinki-zhivotnuh-8-01.jpg" />
+      </div>
+      <div>
+        Ava + Description
+        <div>
+          My posts
+          <div>New posts</div>
+          <div>
+            <div>Post 1</div>
+            <div>Post 2</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
